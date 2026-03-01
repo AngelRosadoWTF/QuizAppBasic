be7a43da780279338770d1fc53b447670091ede0
soyranbo39-web <soy.ranbo39@gmail.com>
2026-03-01 14:22:44 -0600

    Finalizacion de resultados

---
+++ app/src/main/java/Models/RondaPregunta.kt
@@ -0,0 +1,10 @@
+package Models
+
+//data class RondaPregunta(
+////    val pregunta: Pregunta,
+////    val opciones: List<String>,
+////    val indiceOpcionSeleccionada: Int? = null,
+////    val usoPista: Boolean = false,
+////    val indicesEliminados: Set<Int> = setOf()
+//
+//)
